f23538c881acea284cfc012b614ae4dfd965a74f
soroush yousefi <soroush.ysf@gmail.com>
2017-05-10 15:25:00 -0600

    reservation proccess started

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,9 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
+
+
+
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
